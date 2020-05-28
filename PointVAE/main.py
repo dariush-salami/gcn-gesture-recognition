face08363c0ba9d6276588ae4a18659f09c030f0
RaminHasibi <Ramin.Hasibi94@gmail.com>
2020-05-28 20:49:48 +0200

    New main

--- PointVAE/main.py
+++ PointVAE/main.py
@@ -4,11 +4,12 @@ from torch.nn import Sequential as Seq, Linear as Lin, ReLU,LeakyReLU ,BatchNorm
 import torch_geometric.transforms as T
 from torch_geometric.data import DataLoader
 import torch
-from torch_geometric.nn import radius_graph , global_max_pool
+from torch_geometric.nn import radius_graph , global_max_pool, knn_graph
 from neuralnet_pytorch.metrics import chamfer_loss
 from torch_geometric.nn.inits import reset
 import torch.nn.functional as F
 from layer import MuConv, SigmaConv
+from Data import ShapeNet_2048
 
 
 def MLP(channels, batch_norm=True):
@@ -19,51 +20,56 @@ def MLP(channels, batch_norm=True):
 
 
 class Net(torch.nn.Module):
-    def __init__(self, ratio, r):
+    def __init__(self, ratio, k):
         super(Net, self).__init__()
-        self.r = r
-        self.fc1 = MLP([3, 64])
-        self.mu_conv = MuConv(MLP([64, 64]), MLP([64, 20]))
-        self.sig_conv = SigmaConv(MLP([64, 64]), MLP([64, 20]))
-        self.fc3 = Lin(20, 1024)
-        self.fc4 = Lin(1024, 3072)
-
+        self.k = k
+        self.fc1 = MLP([3,  64])
+        self.mu_conv = MuConv(MLP([64, 64]),MLP([64,128]))
+        self.sig_conv = SigmaConv(MLP([64,64]),MLP([64,128]))
+        self.fc3 = Lin(128, 256)
+        self.fc4 = Lin(256, 3*2048)
+        
     def reset_parameters(self):
         reset(self.fc1)
         reset(self.fc3)
         reset(self.fc4)
-
+        
     def forward(self, data):
-        x, pos, batch = data.pos, data.pos, data.batch
-        edge_index = radius_graph(pos, self.r,
-                                  max_num_neighbors=64)
+        x , pos , batch = data.x , data.x, data.batch
+        edge_index = knn_graph(x, self.k, batch, loop=False)
         x = self.fc1(x)
+        
+        
         z_mu = self.mu_conv(x, pos, edge_index)
-        z_sig = self.sig_conv(x, pos, edge_index)
+        z_sig = self.sig_conv(x, pos,edge_index)
         z_mu = global_max_pool(z_mu, batch)
         z_sig = global_max_pool(z_sig, batch)
-        z_sig = z_sig.clamp(max=10)
+        
+        z_sig = z_sig.clamp(max=3)
         if self.training:
             z = z_mu + torch.randn_like(z_sig) * torch.exp(z_sig)
         else:
             z = z_mu
         out = F.relu(self.fc3(z))
         out = self.fc4(out)
-        out = out.reshape((1024 * data.y.size(0), 3))
+        out = out.reshape((2048*data.category.size(0),3))
         return out, z_mu, z_sig, z
 
 
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..',  'data/ModelNet10')
-pre_transform, transform = T.NormalizeScale(), T.FixedPoints(1024)
-train_dataset = ModelNet(path, '10', True, transform, pre_transform)
-test_dataset = ModelNet(path, '10', False, transform, pre_transform)
+#path = osp.join(osp.dirname(osp.realpath(__file__)), '..',  'data/ModelNet10')
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..',  'data/ShapeNet_2048')
+#pre_transform, transform = T.NormalizeScale(), T.FixedPoints(1024)
+#train_dataset = ModelNet(path, '10', True, transform, pre_transform)
+#test_dataset = ModelNet(path, '10', False, transform, pre_transform)
+#train_loader = DataLoader(
+#    train_dataset, batch_size=10, shuffle=True, num_workers=1)
+#test_loader = DataLoader(
+#    test_dataset, batch_size=10, shuffle=False, num_workers=1)
+dataset = ShapeNet_2048(path, split='trainval',categories='Chair')
 train_loader = DataLoader(
-    train_dataset, batch_size=10, shuffle=True, num_workers=1)
-test_loader = DataLoader(
-    test_dataset, batch_size=10, shuffle=False, num_workers=1)
-
+    dataset, batch_size=10, shuffle=True, num_workers=1)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net(0.5, 0.2).to(device)
+model = Net(0.5, 20).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
@@ -76,7 +82,7 @@ def train():
         data = data.to(device)
         optimizer.zero_grad()
         out = model(data)
-        EMD = chamfer_loss(out[0], data.pos)
+        EMD = chamfer_loss(out[0], data.x)
         KLD = -0.5 * torch.mean(
             torch.sum(1 + out[2] - out[1].pow(2) - out[2].exp(),dim=1))
         loss = EMD + KLD
